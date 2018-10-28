7ebff34d81bc720cb1f057d7e6e9dec57ac4285f
Эдуард <work.eriks@gmail.com>
2018-10-29 01:44:00 +0300

    Добавлен фильтр Twig.

--- assets/js/syntaxhighlighter.js
+++ assets/js/syntaxhighlighter.js
@@ -1,9 +1,10 @@
 SyntaxHighlighter.autoloader(
-    ['js','jscript','javascript','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushJScript.js'],
-    ['bash','shell','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushBash.js'],
-    ['css','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushCss.js'],
-    ['xml','html','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushXml.js'],
-    ['sql','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushSql.js'],
-    ['php','/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushPhp.js']
+    ['js', 'jscript', 'javascript', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushJScript.js'],
+    ['bash', 'shell', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushBash.js'],
+    ['css', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushCss.js'],
+    ['xml', 'html', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushXml.js'],
+    ['sql', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushSql.js'],
+    ['php', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushPhp.js'],
+    ['twig', '/plugins/eriks/highcode/assets/vendor/syntaxhighlighter/scripts/shBrushTwig.js']
 );
 SyntaxHighlighter.all();
